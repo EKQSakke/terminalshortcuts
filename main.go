8ccf08f00eda1b94b0ddd00c9abb9acb44c1c69c
Sakari Ekqist <sakari.ekqvist@magicad.com>
2023-05-06 22:52:00 +0300

    Calling with no args returns the path to conf file

--- main.go
+++ main.go
@@ -3,17 +3,37 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) > 2 {
 		fmt.Println("Wrong number of arguments")
 		return
 	}
 
+	if len(os.Args) == 1 {
+		fmt.Println("C:/Users/sakari.ekqvist/source/side/terminalshortcuts/conf.txt")
+		return
+	}
+
 	key := os.Args[1]
+	if key == "edit" {
+		cmd := exec.Command("cmd", "/C start C:/Users/sakari.ekqvist/source/side/terminalshortcuts/conf.txt")
+		err := cmd.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = cmd.Wait()
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
 	file, err := os.Open("C:/Users/sakari.ekqvist/source/side/terminalshortcuts/conf.txt")
 	if err != nil {
 		fmt.Println("Failed to open file:", err)
